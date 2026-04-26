6a27de96e5473fa4b236e7735fa3fddfd4bbf8fe
inncent <innokentiykim90@gmail.com>
2026-04-26 23:32:54 +0400

    auth-service: main app file to start

---
+++ auth-service/app/main.py
@@ -0,0 +1,29 @@
+from app.setup.fastapi_app import create_fastapi_app
+import asyncio
+import uvicorn
+from app.core.config import app_config
+
+
+async def _start_app(port: int) -> None:
+    """Start the FastAPI application."""
+    fastapi_app = create_fastapi_app()
+
+    uvicorn_config = uvicorn.Config(
+        fastapi_app,
+        host="0.0.0.0",
+        port=port,
+        log_config=None,
+        access_log=False,
+    )
+
+    server = uvicorn.Server(uvicorn_config)
+    await server.serve()
+
+
+def start_app(port: int = 8000) -> None:
+    """Start the FastAPI application on the specified port."""
+    asyncio.run(_start_app(port=port))
+
+
+if __name__ == "__main__":
+    start_app(port=app_config.general.app_port)
